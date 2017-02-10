e5c8df69e4c802d3e9a6a5fe11c7f299b743cbaf
Evandro Machado <evandrolmass@gmail.com>
2017-02-10 11:00:11 -0200

    Implemented a basic RESTful API using a Collection to simulate a Database.

--- webapi/src/main/java/com/evandro/controller/CustomerController.java
+++ webapi/src/main/java/com/evandro/controller/CustomerController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,7 +20,7 @@ public class CustomerController {
 	Map<Integer, Customer> customers;
 	Integer nextId = 1;
 	
-	//Negócios
+	//Business Rules
 	private Customer register(Customer customer){
 		if(customers == null){
 			customers = new HashMap<>();
@@ -37,6 +38,20 @@ public class CustomerController {
 		return customers.values();
 	}
 	
+	private void delete(Customer customer){
+		customers.remove(customer.getId());
+	}
+	
+	
+	private Customer findById(Integer id) {
+		return customers.get(id);
+	}
+	
+	private Customer update(Customer customer){
+		customers.put(customer.getId(), customer);
+		return customer;
+	}
+	
 	//Endpoints
 	@RequestMapping(method=RequestMethod.POST, value="/customers", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Customer> registerCustomer(@RequestBody Customer customer){
@@ -49,4 +64,24 @@ public class CustomerController {
 		Collection<Customer> customersFound = findAll();
 		return new ResponseEntity<>(customersFound, HttpStatus.OK);
 	}
+	
+	@RequestMapping(method=RequestMethod.DELETE, value="/customers/{id}")
+	public ResponseEntity<Customer> deleteCustomer(@PathVariable Integer id){
+		Customer customerFound = findById(id);
+		if(customerFound == null){
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		delete(customerFound);
+		
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT, value="/customers", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Customer> updateCustomer(@RequestBody Customer customer){
+		Customer updatedCustomer = update(customer);
+		return new ResponseEntity<>(updatedCustomer, HttpStatus.OK);
+	}
+	
+	
 }
