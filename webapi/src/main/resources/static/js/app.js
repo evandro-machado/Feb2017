d83121934bdb1ec868b2d927726e34e239135a1f
Evandro Machado <evandrolmass@gmail.com>
2017-02-19 22:56:06 -0300

    Creating structure to AngularJS project and started to implement routing.

--- webapi/src/main/resources/static/js/app.js
+++ webapi/src/main/resources/static/js/app.js
@@ -1,76 +1,16 @@
-var appCustomer = angular.module("appCustomer", []);
+var appCustomer = angular.module("appCustomer", ['ngRoute']);
 
-appCustomer.controller("indexController", function($scope, $http) {
-	$scope.name = "Jhon";
-	$scope.lastName = "Doe";
-	$scope.customers = [];
-	$scope.customer = {};
-
-	$scope.loadCustomers = function() {
-		$http({
-			method:'GET',
-			url:'http://localhost:8080/customers'
-		})
-		.then(
-			function (response){
-				$scope.customers = response.data;
-				console.log(response.data);
-				console.log(response.status);
-			}, 
-			function(response){
-				console.log(response.data);
-				console.log(response.status);
-			});
-	}
-
-	$scope.cancelCustomerEdit = function() {
-		$scope.customer = {};
-	}
-
-	$scope.loadCustomers();
-
-	$scope.saveCustomer = function() {
-		$http({
-			method:'POST',
-			url:'http://localhost:8080/customers',
-			data: $scope.customer,
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		})
-		.then(
-			function (response){
-				// $scope.customers.push(response.data);
-				$scope.customer = {};
-				$scope.loadCustomers();
-				console.log(response.data);
-				console.log(response.status);
-			}, 
-			function(response){
-				console.log(response.data);
-				console.log(response.status);
-			});
-	}
-
-	$scope.deleteCustomer = function(customer, position) {
-		$http({
-			method:'DELETE',
-			url:'http://localhost:8080/customers/' + customer.id,
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		})
-		.then(
-			function (response){
-				$scope.customers.splice(position, 1);
-			}, 
-			function(response){
-				console.log(response.data);
-				console.log(response.status);
-			});
-	}
-
-	$scope.editCustomer = function(customer) {
-		$scope.customer = angular.copy(customer);
-	}
+appCustomer.config(function($routeProvider){
+	$routeProvider
+	.when("/customers", {
+		templateUrl: 'view/customer.html',
+		controller: 'controller/customer-controller.js'
+	})
+	.when("/cities", {
+		templateUrl: 'view/city.html'
+	})
+	.when("/states", {
+		templateUrl: 'view/state.html'
+	})
+	.otherwise({redirectTo: '/'});
 });
---
+++ webapi/src/main/resources/static/js/controller/customer-controller.js
@@ -0,0 +1,74 @@
+appCustomer.controller("customerController", function($scope, $http) {
+	$scope.name = "Jhon";
+	$scope.lastName = "Doe";
+	$scope.customers = [];
+	$scope.customer = {};
+
+	$scope.loadCustomers = function() {
+		$http({
+			method:'GET',
+			url:'http://localhost:8080/customers'
+		})
+		.then(
+			function (response){
+				$scope.customers = response.data;
+				console.log(response.data);
+				console.log(response.status);
+			}, 
+			function(response){
+				console.log(response.data);
+				console.log(response.status);
+			});
+	}
+
+	$scope.cancelCustomerEdit = function() {
+		$scope.customer = {};
+	}
+
+	$scope.loadCustomers();
+
+	$scope.saveCustomer = function() {
+		$http({
+			method:'POST',
+			url:'http://localhost:8080/customers',
+			data: $scope.customer,
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+		.then(
+			function (response){
+				// $scope.customers.push(response.data);
+				$scope.customer = {};
+				$scope.loadCustomers();
+				console.log(response.data);
+				console.log(response.status);
+			}, 
+			function(response){
+				console.log(response.data);
+				console.log(response.status);
+			});
+	}
+
+	$scope.deleteCustomer = function(customer, position) {
+		$http({
+			method:'DELETE',
+			url:'http://localhost:8080/customers/' + customer.id,
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+		.then(
+			function (response){
+				$scope.customers.splice(position, 1);
+			}, 
+			function(response){
+				console.log(response.data);
+				console.log(response.status);
+			});
+	}
+
+	$scope.editCustomer = function(customer) {
+		$scope.customer = angular.copy(customer);
+	}
+});
