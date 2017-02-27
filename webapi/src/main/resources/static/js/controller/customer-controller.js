2c4d837f555f3d294557ed0e18fce1fbf7026d2b
Evandro Machado <evandrolmass@gmail.com>
2017-02-26 23:54:33 -0300

    Learned how to use validation.

--- webapi/src/main/resources/static/js/controller/customer-controller.js
+++ webapi/src/main/resources/static/js/controller/customer-controller.js
@@ -28,26 +28,32 @@ appCustomer.controller("customerController", function($scope, $http) {
 	$scope.loadCustomers();
 
 	$scope.saveCustomer = function() {
-		$http({
-			method:'POST',
-			url:'http://localhost:8080/customers',
-			data: $scope.customer,
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		})
-		.then(
-			function (response){
-				// $scope.customers.push(response.data);
-				$scope.customer = {};
-				$scope.loadCustomers();
-				console.log(response.data);
-				console.log(response.status);
-			}, 
-			function(response){
-				console.log(response.data);
-				console.log(response.status);
-			});
+		if($scope.customerForm.$valid){
+			$http({
+				method:'POST',
+				url:'http://localhost:8080/customers',
+				data: $scope.customer,
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			})
+			.then(
+				function (response){
+					// $scope.customers.push(response.data);
+					$scope.cancelCustomerEdit();
+					$scope.loadCustomers();
+					console.log(response.data);
+					console.log(response.status);
+					$scope.customerForm.$setPristine(true);
+				}, 
+				function(response){
+					console.log(response.data);
+					console.log(response.status);
+				}
+			);
+		}else {
+			window.alert("Invalid data.");
+		}
 	}
 
 	$scope.deleteCustomer = function(customer, position) {
