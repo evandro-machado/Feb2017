c49f490a750dc488400b283f79e097f5cd3468b2
Evandro Machado <evandrolmass@gmail.com>
2017-02-27 23:30:21 -0300

    Created backend state API.

--- webapi/src/main/java/com/evandro/controller/StateController.java
+++ webapi/src/main/java/com/evandro/controller/StateController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,6 +31,30 @@ public class StateController {
 	public ResponseEntity<Collection<State>> findAllStates(){
 		Collection<State> statesFound = stateService.findAll();
 		return new ResponseEntity<>(statesFound, HttpStatus.OK);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET, value="/states/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<State> findStateById(@PathVariable Integer id){
+		State state = stateService.findById(id);
+		return new ResponseEntity<>(state, HttpStatus.OK);
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE, value="/states/{id}")
+	public ResponseEntity<State> deleteState(@PathVariable Integer id){
+		State stateFound = stateService.findById(id);
+		
+		if(stateFound == null){
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		stateService.delete(stateFound);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT, value="/states", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<State> updateState(@RequestBody State state){
+		State updatedState = stateService.update(state);
+		return new ResponseEntity<>(updatedState, HttpStatus.OK);
 	}
 	
 	
--- webapi/src/main/resources/static/js/controller/state-controller.js
+++ webapi/src/main/resources/static/js/controller/state-controller.js
@@ -1,3 +1,22 @@
-appCustomer.controller("stateController", function(){
+appCustomer.controller("stateController", function($scope, $http){
+	$scope.states = [];
 
+	$scope.loadStates = function() {
+		$http({
+			method:'GET',
+			url:'http://localhost:8080/states'
+		})
+		.then(
+			function (response){
+				$scope.states = response.data;
+				console.log(response.data);
+				console.log(response.status);
+			}, 
+			function(response){
+				console.log(response.data);
+				console.log(response.status);
+			});
+	}
+
+	$scope.loadStates();
 });
