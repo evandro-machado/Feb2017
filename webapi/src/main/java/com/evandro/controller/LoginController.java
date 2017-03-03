953aca1fbc06efabcf08ea717e69d2e580957bc1
Evandro Machado <evandrolmass@gmail.com>
2017-03-02 23:01:17 -0300

    Working on jwt authentication.

--- webapi/src/main/java/com/evandro/controller/LoginController.java
+++ webapi/src/main/java/com/evandro/controller/LoginController.java
@@ -1,9 +1,11 @@
 package com.evandro.controller;
 
+import java.util.Date;
+
+import javax.servlet.ServletException;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.evandro.model.Operator;
 import com.evandro.service.OperatorService;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
 @RestController
 public class LoginController {
 	
@@ -19,12 +24,44 @@ public class LoginController {
 	private OperatorService userService;
 	
 	@RequestMapping(value="/authenticate", consumes=MediaType.APPLICATION_JSON_VALUE, method=RequestMethod.POST)
-	public ResponseEntity<Operator> autenthicate(@RequestBody Operator operator){
-		System.out.println("Called method authenticate " + operator.getName() + " " + operator.getPassword());
+	public LoginResponse autenthicate(@RequestBody Operator operator) throws ServletException{
+		
+		
+		if (operator.getName() == null || operator.getPassword() == null){
+			throw new ServletException("Name and password are required.");
+		}
 		
 		//Query database
 		Operator authenticatedOperator = userService.findByName(operator.getName());
 		
-		return new ResponseEntity<Operator>(authenticatedOperator, HttpStatus.OK);
+		if(authenticatedOperator == null){
+			throw new ServletException("User not found.");
+		}
+		
+		if(!authenticatedOperator.getPassword().equals(operator.getPassword())){
+			throw new ServletException("Wrong user or password.");
+		}
+		
+		//{token: generatedcode}
+		String token = Jwts.builder()
+				.setSubject(authenticatedOperator
+				.getName())
+				.signWith(SignatureAlgorithm.HS512, "banana")
+				.setExpiration(new Date(System.currentTimeMillis() + 1 * 60 * 1000))
+				.compact();
+
+		return new LoginResponse(token);
+	}
+	
+	private class LoginResponse{
+		String token;
+		
+		public LoginResponse(String token){
+			this.token = token;
+		}
+		
+		public String getToken(){
+			return token;
+		}
 	}
 }
